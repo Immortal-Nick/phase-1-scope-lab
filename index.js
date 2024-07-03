46ef2fb9547ba5b93235e75f592ab05fd03049b3
Nixon <kiplangatnixon28@gmail.com>
2024-07-03 17:59:31 +0300

    creates scopes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function getCustomerName() {
+    return customerName;
+  }
+  
+  function setBestCustomer() {
+    bestCustomer = 'not bob'; // This will declare a global variable
+  }
+  
+  // Function to overwrite the global bestCustomer variable
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  
+  // Function to return bestCustomer
+  function getBestCustomer() {
+    return bestCustomer;
+  }
+  
+  const leastFavoriteCustomer = 'faith';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer='jane';
+  }
